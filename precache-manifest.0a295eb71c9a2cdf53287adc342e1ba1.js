d2cb3785d83929fec567e587bb9f49c42c4ce231
modz <Ltmodz@github.com>
2020-02-10 19:24:51 +0200

    Updates

---
+++ precache-manifest.0a295eb71c9a2cdf53287adc342e1ba1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4509c960c7c55ab3be5faa2b5ee55448",
+    "url": "/Wolfy/index.html"
+  },
+  {
+    "revision": "700f1278b4af1e6ce320",
+    "url": "/Wolfy/static/css/main.b3f58e3a.chunk.css"
+  },
+  {
+    "revision": "e4b99eca64ca20447bba",
+    "url": "/Wolfy/static/js/2.3a931138.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Wolfy/static/js/2.3a931138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db967af966158f07fbfe",
+    "url": "/Wolfy/static/js/3.fc26ec0e.chunk.js"
+  },
+  {
+    "revision": "c80f8f3c430ab7487dc3",
+    "url": "/Wolfy/static/js/4.39b7358d.chunk.js"
+  },
+  {
+    "revision": "68bb9ccc290acee6e5bb",
+    "url": "/Wolfy/static/js/5.78587df2.chunk.js"
+  },
+  {
+    "revision": "91b76b50c846e825f0ef",
+    "url": "/Wolfy/static/js/6.16102ff5.chunk.js"
+  },
+  {
+    "revision": "700f1278b4af1e6ce320",
+    "url": "/Wolfy/static/js/main.12aad567.chunk.js"
+  },
+  {
+    "revision": "2938bbc936d8f9c4b0f9",
+    "url": "/Wolfy/static/js/runtime-main.d784a1ee.js"
+  },
+  {
+    "revision": "2f5b01b916be1db348cda8856a338269",
+    "url": "/Wolfy/static/media/gift.2f5b01b9.svg"
+  },
+  {
+    "revision": "cc1c5f3d6700a2dfc579e9d6dc00631e",
+    "url": "/Wolfy/static/media/jacket.cc1c5f3d.png"
+  },
+  {
+    "revision": "03ddb98b4eca9638398e95a869d71566",
+    "url": "/Wolfy/static/media/jeans.03ddb98b.png"
+  },
+  {
+    "revision": "145deff6c4a437807faa9af6666c1f12",
+    "url": "/Wolfy/static/media/pullover.145deff6.png"
+  },
+  {
+    "revision": "188889f1f6c1dd17ed0e27640bec679e",
+    "url": "/Wolfy/static/media/shoes.188889f1.png"
+  },
+  {
+    "revision": "46c3c4432819c878a4e4bd6ec9cb1766",
+    "url": "/Wolfy/static/media/suit.46c3c443.png"
+  },
+  {
+    "revision": "81c679241d5c1a864926a582abf99fcb",
+    "url": "/Wolfy/static/media/towel.81c67924.png"
+  },
+  {
+    "revision": "6b046dff4a7a726a99da6d8128d420a2",
+    "url": "/Wolfy/static/media/wolf.6b046dff.svg"
+  }
+]);
